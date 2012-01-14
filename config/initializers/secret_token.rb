bdf796c485b03397a379a917535bbe40631e267b
Jim Knight <jim.knight@lavatech.com>
2012-01-13 19:08:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprinkle::Application.config.secret_token = '8b2f3537695ca16d70ee1affbd0aa662c94a99373dc5fdd95b96d34ad02173977b7480a9775f8ed0fca57599458971ab49b577a301184851fde55fcfb6fb5c27'
