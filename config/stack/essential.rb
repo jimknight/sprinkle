f448fd5d43b074d761cae844ab38aa908f767748
Jim Knight <jim.knight@lavatech.com>
2012-01-14 15:47:31 -0500

    automatic pre-deployment commit

--- config/stack/essential.rb
+++ config/stack/essential.rb
@@ -2,9 +2,10 @@ package :build_essential do
   description 'Build tools'
   apt 'build-essential' do
     pre :install, 'apt-get update'
+    pre :install, 'apt-get install libyaml-dev -y'
     pre :install, 'apt-get install vim -y'
   end
-  verify do
-    has_executable 'vim'
-  end
+  # verify do
+  #   has_executable 'vim'
+  # end
 end
