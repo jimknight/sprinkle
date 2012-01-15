b4749e24751e1aca2ba37d53b7fee6979ca6e434
Jim Knight <jim.knight@lavatech.com>
2012-01-14 21:11:34 -0500

    automatic pre-deployment commit

--- config/stack/apache.rb
+++ config/stack/apache.rb
@@ -18,18 +18,44 @@ package :passenger, :provides => :appserver do
   
  gem_version = '3.0.11'
   
-  load_file = '/etc/apache2/mods-available/passenger.load'
-  conf_file = '/etc/apache2/mods-available/passenger.conf'
+  # load_file = '/etc/apache2/mods-available/passenger.load'
+  # conf_file = '/etc/apache2/mods-available/passenger.conf'
   
-  gem 'passenger' do
+  binaries = %w(passenger-config passenger-install-nginx-module passenger-install-apache2-module passenger-make-enterprisey passenger-memory-stats passenger-spawn-server passenger-status passenger-stress-test)
+
+   gem 'passenger', :version => gem_version do
+     binaries.each {|bin|}
+  
+  
+  # gem 'passenger' do
     # cleanup any other versions of passenger we had installed
-    pre :install, 'gem uninstall passenger -a -x || true'
-    pre :install, 'rm -f /etc/apache2/mods-available/passenger.*'
+    # pre :install, 'gem uninstall passenger -a -x || true'
+    # pre :install, 'rm -f /etc/apache2/mods-available/passenger.*'
     post :install, 'passenger-install-apache2-module --auto'
-    post :install, "echo \"LoadModule passenger_module `gem environment gemdir`/gems/passenger-#{gem_version}/ext/apache2/mod_passenger.so\" | sudo tee #{load_file}"
-       post :install, "echo \"PassengerRoot `gem environment gemdir`/gems/passenger-#{gem_version}\" | sudo tee #{conf_file}"
-       post :install, "echo \"PassengerRuby /usr/local/bin/ruby\" | sudo tee -a #{conf_file}"
-       post :install, 'a2enmod passenger'
+    # Create the passenger conf file
+    # post :install, 'mkdir -p /etc/apache2/extras'
+    # post :install, 'touch /etc/apache2/extras/passenger.conf'
+    # post :install, 'echo "Include /etc/apache2/extras/passenger.conf"|sudo tee -a /etc/apache2/apache2.conf'
+    # 
+    # post :install, "echo \"LoadModule passenger_module `gem environment gemdir`/gems/passenger-#{gem_version}/ext/apache2/mod_passenger.so\" | sudo tee #{load_file}"
+    #    post :install, "echo \"PassengerRoot `gem environment gemdir`/gems/passenger-#{gem_version}\" | sudo tee #{conf_file}"
+    #    post :install, "echo \"PassengerRuby /usr/local/bin/ruby\" | sudo tee -a #{conf_file}"
+    #    post :install, 'a2enmod passenger'
+       
+       # Create the passenger conf file
+       post :install, 'mkdir -p /etc/apache2/extras'
+       post :install, 'touch /etc/apache2/extras/passenger.conf'
+       post :install, 'echo "Include /etc/apache2/extras/passenger.conf"|sudo tee -a /etc/apache2/apache2.conf'
+
+       [%Q(LoadModule passenger_module /usr/local/lib/ruby/gems/1.9.1/gems/passenger-#{gem_version}/ext/apache2/mod_passenger.so),
+       %Q(PassengerRoot /usr/local/lib/ruby/gems/1.9.1/gems/passenger-#{gem_version}),
+       %q(PassengerRuby /usr/local/bin/ruby),
+       %q(RailsEnv production)].each do |line|
+         post :install, "echo '#{line}' |sudo tee -a /etc/apache2/extras/passenger.conf"
+       end
+
+       # Restart apache to note changes
+       
     post :install, '/etc/init.d/apache2 restart'
   end
   
